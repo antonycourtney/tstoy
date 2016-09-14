241aa15cc8ea20c2d86d93c76d1d6e4e4ab4db89
Antony Courtney <antony.courtney@gmail.com>
2016-09-14 15:03:29 -0700

    Basic svg test working under TypeScript

--- src/xstreamSvgTest.ts
+++ src/xstreamSvgTest.ts
@@ -1,27 +1,27 @@
 import xs from 'xstream'
 import fromEvent from 'xstream/extra/fromEvent'
-const svgNS = "http://www.w3.org/2000/svg"
 
 const mkCircle = ([cx,cy]) => {
   console.log([cx,cy])
-  const circ = document.createElementNS(svgNS, 'circle')
+  const circ = document.createElementNS("http://www.w3.org/2000/svg", 'circle')
   circ.setAttribute('cx', cx)
   circ.setAttribute('cy', cy)
   circ.setAttribute('r', '50')
-  circ.style = 'stroke: red; fill: red'
+  circ.style.stroke = 'red'
+  circ.style.fill = 'red'
   return circ
 }
 
 // Wrap an SVG element elem in a <g> with id 'scene':
-const mkScene = elem => {
-  const nextScene = document.createElementNS(svgNS, 'g')
+const mkScene = (elem: SVGElement )  => {
+  const nextScene = document.createElementNS("http://www.w3.org/2000/svg", 'g')
   nextScene.id = 'scene'
   nextScene.appendChild(elem)
   return nextScene
 }
 
 // imperative action to update the DOM:
-const updateScene = nextScene => {
+const updateScene = (nextScene: SVGElement) => {
   const prevScene = document.getElementById('scene')
   const svgElem = document.getElementById('app-svg')
   svgElem.replaceChild(nextScene, prevScene)
@@ -48,7 +48,7 @@ const main = () => {
     error:err => console.error(err),
     complete: () => console.log('completed')
   })
-  
+
 }
 
 window.onload = main
